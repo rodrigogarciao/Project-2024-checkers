9fd1ff93cc0c47c1ed5db3874c0cfd6f8b705082
andrenavd <165317049+andrenavd@users.noreply.github.com>
2024-04-08 17:59:14 -0600

    Add files via upload

---
+++ Checkers.py
@@ -0,0 +1,7 @@
+#Explain the game 
+#The game is checkers is made from a board of 8x8 made up by white and black squares 
+#In one side there 12 black pieces and they are placed diagonal to each other over the first three lines on the black squares
+#On the other side of the black pieces theres the white pieces where they will be placed like the black pieces
+#The pieces can only move one square in diagonal order and you take / eliminate the others pieces by jumpin over the other piece and landing in the square behind the opponents piece. 
+#The goal is to eliminate all of opponents pieces
+#a master piece that can jump forward and backwards can appear once a piece reaches the other side
