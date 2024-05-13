0cb6cd8264cded9cda138d2c2afb2f01bb48c908
rodrigogarciao <165348510+rodrigogarciao@users.noreply.github.com>
2024-05-13 01:20:36 -0400

    Update _main_.py

--- checkers/__main__.py
+++ checkers/__main__.py
@@ -1,10 +1,3 @@
-from board import new_board, print_board
-from actions import switch_player, is_winner, is_valid_move, make_move
-
-board = new_board()
-player1 = 'X'
-player2 = 'O'
-current_player = player1
 
 from board import new_board, print_board
 from actions import is_winner, is_valid_move, make_move, switch_player
@@ -12,6 +5,10 @@ from actions import is_winner, is_valid_move, make_move, switch_player
 board = new_board()
 current_player = 'X'
 
+print("Welcome to Tic Tac Toe!")
+print("Checkers is a game that is played with 2 persons and in this case is the only way to play it ")
+print("You can only do movements in diagonal and the point of this game is for you to eat the O or X that the player your going against is using ")
+print("You win when you have eaten all the pieces or the other player can`t make any movementes anymore ")
 print("Enter positions in the format 'row,col'. For example, '3,4' represents row 3, column 4.")
 
 while not is_winner(board, current_player):
@@ -26,9 +23,9 @@ while not is_winner(board, current_player):
         make_move(board, start, end, current_player)
         current_player = switch_player(current_player)
     else:
-        print("Thats incorrect, try again")
+        print("That's incorrect, try again")
 
 print_board(board)
-print(f"Player {current_player} wins by so much!")
+print(f"Player {current_player} wins! Congratulations!")
 
 
